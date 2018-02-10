29a754f68371839bbb09f6a66495f6a1a392a465
gitjchan <34874538+gitjchan@users.noreply.github.com>
2018-02-09 17:49:48 -0800

    proj01
    Drumkit

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+// Functions
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+
+  // Audio code
+  if (!audio) return; // if keyCode doesn't match dont run - cancels out 'null' values
+  audio.currentTime = 0; // resets time to 0
+  audio.play(); // play the animation
+
+  // Animation of CSS
+  key.classList.add("playing"); // Adding a class
+};
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return; // if event.propertyName doesn't equal 'transform', ignore
+  this.classList.remove("playing"); // Remove the class
+}
+
+const keys = document.querySelectorAll(".key"); //find all elements with '.keys' class
+
+// loop through each 'key' transitionend and execute 'removeTransition' function
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
+
+window.addEventListener("keydown", playSound);
+
+
